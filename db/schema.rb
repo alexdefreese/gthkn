72661e25ac4c90af9d6ea0164736ebcf456a4c86
alexdefreese <alexdefreese@gmail.com>
2012-01-20 11:53:00 -0500

    Created Users model

--- db/schema.rb
+++ db/schema.rb
@@ -9,6 +9,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20120120161018) do
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.boolean  "officer"
+    t.boolean  "initiate"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
