abe15b70f3811b9163e9ca4986339885e127df7b
Alex DeFreese <alexdefreese@gmail.com>
2012-02-01 10:20:26 -0500

    Upgraded routes.rb and Gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,66 +1,25 @@
-ActionController::Routing::Routes.draw do |map|
-  # The priority is based upon order of creation: first created -> highest priority.
-
-  # Sample of regular route:
-  #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   map.resources :products
-
-  # Sample resource route with options:
-  #   map.resources :products, :member => { :short => :get, :toggle => :post }, :collection => { :sold => :get }
-
-  # Sample resource route with sub-resources:
-  #   map.resources :products, :has_many => [ :comments, :sales ], :has_one => :seller
+Gthkn::Application.routes.draw do
+  resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
+  resources :blogposts, :only => [:create, :edit, :update, :destroy]
   
-  # Sample resource route with more complex sub-resources
-  #   map.resources :products do |products|
-  #     products.resources :comments
-  #     products.resources :sales, :collection => { :recent => :get }
-  #   end
-
-  # Sample resource route within a namespace:
-  #   map.namespace :admin do |admin|
-  #     # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
-  #     admin.resources :products
-  #   end
-
-  # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
-
-  # See how all your routes lay out with "rake routes"
-
-  # Install the default routes as the lowest priority.
-  # Note: These default routes make all actions in every controller accessible via GET requests. You should
-  # consider removing or commenting them out if you're using named routes and resources.
+  match '/signin' => 'sessions#new', :as => :signin
+  match '/signout' => 'sessions#destroy', :as => :signout
   
-  map.resources :users
-  map.resources :sessions, :only => [:new, :create, :destroy]
-  map.resources :blogposts, :only => [:create, :edit, :update, :destroy]
+  match '/admin/user_admin' => 'users#user_admin', :as => :user_admin
   
-  map.signin  '/signin',      :controller => 'sessions', :action => 'new'
-  map.signout '/signout',     :controller => 'sessions', :action => 'destroy'
+  match '/:controller(/:action(/:id))'
   
-  map.user_admin '/admin/user_admin', :controller => 'users', :action => 'user_admin'
+  match '/signup' => 'users#new', :as => :signup
   
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
+  match '/new_blogpost' => 'blogposts#new', :as => :new_blogpost
   
-  map.signup  '/signup',      :controller => 'users', :action => 'new'
-  map.new_blogpost '/new_blogpost', :controller => 'blogposts', :action => 'new'
+  match '/' => 'pages#home'
+  match '/about' => 'pages#about', :as => :about
+  match '/events' => 'pages#events', :as => :events
+  match '/corporate' => 'pages#corporate', :as => :corporate
+  match '/officers' => 'pages#officers', :as => :officers
+  match '/contact' => 'pages#contact', :as => :contact
   
-  map.root                    :controller => 'pages', :action => 'home'
-  map.about     '/about',     :controller => 'pages', :action => 'about'
-  map.events    '/events',    :controller => 'pages', :action => 'events'
-  map.corporate '/corporate', :controller => 'pages', :action => 'corporate'
-  map.officers  '/officers',  :controller => 'pages', :action => 'officers'
-  map.contact   '/contact',   :controller => 'pages', :action => 'contact'
-  map.admin     '/admin',     :controller => 'pages', :action => 'admin'
-  
-
+  match '/admin' => 'pages#admin', :as => :admin
 end
