fc93c555356e01ecbc39e7c631bb9115dde577a0
Alex DeFreese <alexdefreese@gmail.com>
2012-02-01 12:04:58 -0500

    Blogposts now render properly

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,7 @@ class PagesController < ApplicationController
   
   def home
     @title = "Home"
-    @blogposts = Blogpost.paginate(:page => params[:page])
+    @blogposts = Blogpost.paginate(:page => params[:page], :per_page => 10)
   end
 
   def contact
