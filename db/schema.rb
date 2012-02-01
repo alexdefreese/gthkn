12e881c7b7b8f5ff2d6dc76e94e4cb920f98425c
Alex DeFreese <alexdefreese@gmail.com>
2012-02-01 15:30:45 -0500

    Home page displays Announcements and Add new post when there are no posts.

--- db/schema.rb
+++ db/schema.rb
@@ -40,5 +40,6 @@ ActiveRecord::Schema.define(:version => 20120201171555) do
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
