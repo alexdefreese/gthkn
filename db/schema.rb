1d22e2e0e3bbdd1eee220d97ebb9be7421b66a9d
Alex DeFreese <alexdefreese@gmail.com>
2012-09-25 22:45:52 -0400

    DB changes

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -33,10 +34,10 @@ ActiveRecord::Schema.define(:version => 20120201171555) do
     t.string   "officer_position"
     t.string   "remember_token"
     t.boolean  "member"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
