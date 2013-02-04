5f8ce43632ccd6163b95eaf50cc7733569f6bfc1
Alex DeFreese <alexdefreese@gmail.com>
2013-02-04 17:43:44 -0500

    Dammit. Set to blogposts not blogpost in recent migration.

--- db/migrate/20130204223905_increase_blogpost_content_size.rb
+++ db/migrate/20130204223905_increase_blogpost_content_size.rb
@@ -1,6 +1,6 @@
 class IncreaseBlogpostContentSize < ActiveRecord::Migration
   def up
-    change_column :blogpost, :content, :text, limit: 5000
+    change_column :blogposts, :content, :text, limit: 5000
   end
 
   def down
