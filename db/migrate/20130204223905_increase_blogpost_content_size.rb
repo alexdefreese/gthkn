d6ab2c8c1d91bceec60740ca739ef18c1817ab45
Alex DeFreese <alexdefreese@gmail.com>
2013-02-04 17:48:04 -0500

    Properly configured for text type

--- db/migrate/20130204223905_increase_blogpost_content_size.rb
+++ db/migrate/20130204223905_increase_blogpost_content_size.rb
@@ -1,6 +1,6 @@
 class IncreaseBlogpostContentSize < ActiveRecord::Migration
   def up
-    change_column :blogposts, :content, :text, limit: 5000
+    change_column :blogposts, :content, :text
   end
 
   def down
