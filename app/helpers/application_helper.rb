0e27812eae338b758bf035193592f5fa76ef7053
Alex DeFreese <alexdefreese@gmail.com>
2013-02-14 13:12:45 -0500

    #7 Restored authorization filters

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,23 @@ module ApplicationHelper
       "#{base_title} | #{h(@title)}"
     end
   end
+  
+  def deny_access
+    flash[:notice] = "Please sign in to access this page"
+    redirect_to signin_path
+  end
+  
+  def authenticate
+    deny_access unless user_signed_in?
+  end
+  
+  def current_user_is_officer?
+    return false if !user_signed_in?
+    current_user.officer
+  end
+  
+  def deny_unless_officer
+    redirect_to root_path unless current_user_is_officer?
+  end
+  
 end
