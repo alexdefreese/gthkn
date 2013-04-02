8d42ddb93b41c48af65be561af321d0859d00f34
Alex DeFreese <alexdefreese@gmail.com>
2013-04-01 23:52:21 -0400

    #17 Sign up works, except password is not stored for some reason

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,30 +19,5 @@ class UsersController < ApplicationController
     @title = "Admin Search Results"
   end
   
-  def create
-    @user = User.new(params[:user])
-    if @user.save
-      sign_in @user
-      flash[:success] = "Thank you for signing up."
-      redirect_to @user
-    else
-      @title = "Sign up"
-      render 'new'
-    end
-  end
-  
-  private
-    
-    
-    def correct_user
-      @user = User.find(params[:id])
-      redirect_to(root_path) unless current_user?(@user)
-    end
-    
-    def correct_user_or_officer
-      if !current_user_is_officer?
-        correct_user
-      end
-    end
 
 end
