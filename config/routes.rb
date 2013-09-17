ea8efb1fccb063dd4fc0b85ce153754e3b411b27
Alex DeFreese <alexdefreese@gmail.com>
2013-09-16 20:28:59 -0400

    Stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Gthkn::Application.routes.draw do
 
   resources :wikipages do
-    resources :wiki_revisions, only: [:new, :create]
+    resources :wiki_revisions, only: [:new, :create, :index]
   end
 
   devise_for :users
@@ -11,21 +11,21 @@ Gthkn::Application.routes.draw do
   resources :events
   resources :initiation_requirements, only: [:new, :create, :update]
   resources :auth_codes
-  
+
   match '/admin/user_admin' => 'users#user_admin', :as => :user_admin
   match '/admin/user_admin/search' => 'users#user_admin_search', :as => :user_admin_search
   match '/admin/initiation' => 'promotions#initiation', as: :initiation
 
-  match '/promotion/member_request/:id' => 'promotions#request_member_promotion', 
+  match '/promotion/member_request/:id' => 'promotions#request_member_promotion',
     as: :member_promote_request
   match '/promotion/member' => 'promotions#member_promote', as: :member_promote
-  
+
   match '/:controller(/:action(/:id))'
-  
+
   match '/signup' => 'users#new', :as => :signup
-  
+
   match '/new_blogpost' => 'blogposts#new', :as => :new_blogpost
-  
+
   root :to => 'pages#home'
   match '/about' => 'pages#about', :as => :about
   match '/eventlist' => 'pages#events', :as => :eventlist
@@ -33,6 +33,6 @@ Gthkn::Application.routes.draw do
   match '/officers' => 'pages#officers', :as => :officers
   match '/contact' => 'pages#contact', :as => :contact
   match '/blog' => 'blogposts#index', :as => :blog
-  
+
   match '/admin' => 'pages#admin', :as => :admin
 end
